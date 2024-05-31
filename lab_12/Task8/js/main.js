5ed747aedf85414d53824b3bd7c50e77b4149aa4
DirFay <158141681+dirfay@users.noreply.github.com>
2024-06-01 00:53:23 +0300

    Lab 12 Done

---
+++ lab_12/Task8/js/main.js
@@ -0,0 +1,5 @@
+const secondsInHour = 60 * 60;
+const secondsInDay = (60 * 60) * 24;
+const secondsInMonth = ((60 * 60) * 24) * 31;
+
+alert("В годині: " + secondsInHour + " секунд\nВ добі: " + secondsInDay + " секунд\nВ місяці: " + secondsInMonth + " секунд");
